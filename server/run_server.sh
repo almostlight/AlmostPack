7c2eb5d31639d97812db510e943d410aaad3ec54
almostlight <almostlight@protonmail.com>
2023-05-03 15:14:40 -0400

    Update run_server.sh

--- server/run_server.sh
+++ server/run_server.sh
@@ -5,26 +5,24 @@ PACKDIR="$HOME/Documents/AlmostPack/"
 LOGFILE="$HOME/run_server.log" 
 
 log () {
-    echo -e "$1 at $(date)" >> $LOGFILE 
+    echo -e "$1 at $(date)" >> "$LOGFILE" 
 }
 
-if [ -e "$PACKDIR" ]
-then
-    cd $PACKDIR     &&      git pull
-    log "Pulled git repo"
-    if [ -e "$MCDIR" ]
-    then
-            cp -rf $PACKDIR/server/mods/ $MCDIR/mods/
-    else
-        log "Server dir not found" 
-    fi 
+if [ -e "$PACKDIR" ] && [ -e "$MCDIR" ]; then
+    cd "$PACKDIR" && git pull 
+    log "Pulled git repo" 
+    DIFF_OUTPUT=$(diff -r "$PACKDIR/server/mods" "$MCDIR/mods") 
 else 
-    log "Pack dir not found" 
+    log "Directory not found" 
 fi
 
+if [[ -n $DIFF_OUTPUT ]]; then
+    ## this may leave deprecated mods. we shall see. 
+    cp -rf "$PACKDIR"/server/mods/ "$MCDIR"/ 
+    log "Updated mods on server"
+fi
 
-# option to pass nogui argument to $1
-cd $MCDIR       &&      bash ./run.sh $1 &
+## option to pass nogui argument to $1
+cd "$MCDIR" && bash ./run.sh "$1" &
 
 log "Ran RustBox" 
-
