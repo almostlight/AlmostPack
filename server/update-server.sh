275cf167399e30943b6374d7faad581e6b9aebf2
almostlight <almostlight@protonmail.com>
2023-05-05 10:42:19 -0400

    qol improvements

--- server/update-server.sh
+++ server/update-server.sh
@@ -1,26 +1,32 @@
 #! /bin/bash
-## Run this script as sudo 
+## check for sudo || run this script as sudo 
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+## cd into the script's source directory 
+cd "${0%/*}" && pwd  
 
 LOGFILE="/opt/minecraft/server.log" 
-PACKDIR="$HOME/Documents/AlmostPack" 
+PACKDIR="${0%/*}" ## AlmostPack/server/ 
 MCDIR="/opt/minecraft/$1" 
 
 log () {
-    echo -e "$1 at $(date)" >> "$LOGFILE" 
-}
+    echo "$1" 
+    echo -e "[$(date)] $1" >> "$LOGFILE" 
+}   
 
 if [ -e "$PACKDIR" ] && [ -e "$MCDIR" ]; then
     cd "$PACKDIR" && git pull 
     log "Pulled git repo" 
-    DIFF_OUTPUT=$(diff -r "$PACKDIR/server/mods" "$MCDIR/mods") 
+    DIFF_OUTPUT=$(diff -r "$PACKDIR/mods" "$MCDIR/mods") 
 else 
     log "Directory not found" 
 fi
 
 if [[ -n $DIFF_OUTPUT ]]; then
-    ## this may leave deprecated mods. we shall see. 
-    cp -rf "$PACKDIR/server/mods/" "$MCDIR/" 
-    cp "$PACKDIR/server/run_server.sh" /opt/minecraft/ 
-    cp "$PACKDIR/server/minecraft@.service" /etc/systemd/system/
+    ## this may leave deprecated mods. 
+    cp -rf "$PACKDIR/mods/" "$MCDIR/" 
+    cp "$PACKDIR/server-icon.png" "$MCDIR/" 
+    cp "$PACKDIR/minecraft@.service" /etc/systemd/system/
     log "Updated files on server"
-fi
+else 
+    log "Server files already on newest version"    
+fi 
